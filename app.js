ef5d6db986a3ab980d82e6a1a02a0a21826aa441
Upendra9642 <upendrarajana365@gmail.com>
2026-08-04 15:38:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const BASE_URL = "https://api.exchangerate-api.com/v4/latest";
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn= document.querySelector("form button");
+const fromcurr =document.querySelector( ".from select");
+const tocurr =document.querySelector( ".to select");
+const msg =document.querySelector( ".msg");
+for (let select of dropdowns) {
+    for (let currCode in countryList) {  // ✅ added 'let'
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;  // ✅ corrected casing
+        newOption.value = currCode;
+        if (select.name==="from" && currCode==="USD"){
+            newOption.selected="selected";
+        } else if (select.name==="to" && currCode==="INR"){
+            newOption.selected="selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt)=>{
+       updateFlag(evt.target);
+    });
+}
+const updateFlag =(element)=>{
+   let currCode = element.value;
+   let countryCode=countryList[currCode];
+   let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
+   let img=element.parentElement.querySelector("img");
+   img.src=newSrc;
+}
+btn.addEventListener("click",async(evt)=>{
+ evt.preventDefault();
+ let amount = document.querySelector(".amount input");
+ let amtVal = parseFloat(amount.value);
+ if (amtVal==="" || amtVal < 1){
+    amtVal =1;
+    amount.value=1;
+ }
+const URL = `${BASE_URL}/${fromcurr.value}`;
+let response = await fetch(URL);
+let data = await response.json();
+let rate = data.rates[tocurr.value]; // this causes error if data.rates is undefined
+ 
+ 
+ let finalAmount = amtVal * rate.toFixed(2);
+ msg.innerText=`${amtVal} ${fromcurr.value}= ${finalAmount} ${tocurr.value}`;
+});
